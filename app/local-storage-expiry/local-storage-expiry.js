6ca5ab8c40ec5a951008945cfd003b882082f672
Atish Raina <atish.raina91@gmail.com>
2023-09-17 15:53:43 +0530

    Modify the local storage expiry code

--- app/local-storage-expiry/local-storage-expiry.js
+++ app/local-storage-expiry/local-storage-expiry.js
@@ -1,38 +1,40 @@
 //https://learnersbucket.com/examples/interview/localstorage-with-expiry/#:~:text=Set%20the%20expiry%20date%20to,in%20the%20original%20local%20storage.&text=Likewise%2C%20while%20getting%20the%20value,the%20entry%20and%20return%20null.
 
-const setLocalStorage = () => {
-    return (key, val) => {
-        const storeTime = Date.now();
-        let value = [];
-        value.push(val);
-        value.push(storeTime);
-        localStorage.setItem(key, value);
-    }
+const setLocalStorage = (key, value, expiry) => {
+    const maxAge = Date.now() + expiry;
+    let data = {};
+    data['_value'] = value;
+    data['_expiry'] = maxAge;
+    localStorage.setItem(key, JSON.stringify(data));
 }
 
-const getLocalStorgae = () => {
-    return (key, expiry) => {
-        let value = localStorage.getItem(key);
-        if(value) {
-            value = value.split(',')
-            const storedTime = value[1]; 
-            if((Date.now() - storedTime) >= expiry) {
-                localStorage.removeItem(key);
-                return 'sorry the item is expired';
-            } else {
-                return localStorage.getItem(key)[0];
-            }
+const getLocalStorgae = (key) => {
+    let data = JSON.parse(localStorage.getItem(key));
+    if(data) {
+        const expiry = data['_expiry']; 
+        if(Date.now() >= expiry) {
+            localStorage.removeItem(key);
+            return 'sorry the item is expired';
+        } else {
+            return data['_value'];
         }
-        
+    } else {
+        return('the requested key is not found');
     }
 }
 
-const setStorage = setLocalStorage();
-setStorage('x', 'y');
-const getStorage = getLocalStorgae();
-const getValue = getStorage;
-// alert(getValue);
-console.log(getValue('x', '2000'));
+setLocalStorage('x', 'y', 5000);
+console.log(getLocalStorgae('t'));
+console.log(getLocalStorgae('x'));
 setTimeout(() => {
-    console.log(getValue('x', '2000'));
+    console.log(getLocalStorgae('x'));
 }, 3000);
+
+setTimeout(() => {
+    console.log(getLocalStorgae('x'));
+}, 5000);
+
+setTimeout(() => {
+    console.log(getLocalStorgae('x'));
+}, 6000);
+
